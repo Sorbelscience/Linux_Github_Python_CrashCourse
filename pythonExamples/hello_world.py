e6abfa430749d3cedbc12a39ff80d4e8a33d6397
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2023-06-01 13:39:43 -0700

    backing up coursework

---
+++ pythonExamples/hello_world.py
@@ -0,0 +1,12 @@
+'''
+This is a block comment. The lines of text here will not be ran.
+@author: Dan Blanchette
+@date: May 14, 2023
+@decription: This program will print hello world to the terminal
+'''
+
+# <--- this indicates to the code that is is a comment and will not be ran as part of your program
+
+# To run this code, enter the following command into the terminal:
+# python3 file_name.py in this case, hello_world.py.
+print("Hello World")
