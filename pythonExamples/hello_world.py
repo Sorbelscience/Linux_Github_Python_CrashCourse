9a5665199210b4e5b46dd5890093ec77603f478b
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2024-05-17 14:01:34 -0700

    updated this example file for the mainfunction.

--- pythonExamples/hello_world.py
+++ pythonExamples/hello_world.py
@@ -1,8 +1,9 @@
 '''
 This is a block comment. The lines of text here will not be ran.
 @author: Dan Blanchette
-@date: May 14, 2023
+@date: May 17, 2024
 @decription: This program will print hello world to the terminal
+and hello from main when the main program function is invoked and ran.
 '''
 
 # <--- this indicates to the code that is is a comment and will not be ran as part of your program
@@ -10,3 +11,11 @@ This is a block comment. The lines of text here will not be ran.
 # To run this code, enter the following command into the terminal:
 # python3 file_name.py in this case, hello_world.py.
 print("Hello World")
+
+# example of how to setup a main program function in python
+
+def main():
+    print("Hello from main")
+
+if __name__ == "__main__":
+    main()
